7ad4433c03d5d6f4cc6d5284b18956efae5a5333
Memphiz <memphis@machzwo.de>
2020-09-26 21:43:01 +0200

    Added interface for being notified about function key presses

---
+++ JukeCore/IFunctionKeyEvents.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JukeCore
+{
+    /// <summary>
+    /// Interface for allowing to register for events when function keys are pressed in console
+    /// </summary>
+    public interface IFunctionKeyEvents
+    {
+        event EventHandler<ConsoleKey> OnFunctionKeyPressed;
+    }
+}
