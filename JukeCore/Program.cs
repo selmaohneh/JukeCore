fa02fa1f5dd4bd157ef400cf7ea188a3f4cb1f8f
Memphiz <memphis@machzwo.de>
2020-10-03 22:27:20 +0200

    Program: - adaptions for changed ctors, create mqtt service if power args say so

--- JukeCore/Program.cs
+++ JukeCore/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Device.Gpio;
 using System.IO.Abstractions;
-using System.Linq;
 using System.Threading.Tasks;
 using LibVLCSharp.Shared;
+using MQTTnet;
+using MQTTnet.Client.Options;
 using PowerArgs;
 
 namespace JukeCore
@@ -30,6 +32,7 @@ namespace JukeCore
 
             try
             {
+                var dataModel = new JukeCoreDataModel();
                 
                 _console = new ConsoleWrapper();
                 _console.WriteLine("JukeCore started!");
@@ -44,21 +47,24 @@ namespace JukeCore
                 var fileSystem = new FileSystem();
                 using var libVlc = new LibVLC();
                 _playlist = new Playlist(_console);
-                _mediaPlayer = new MediaPlayerWrapper(new MediaPlayer(libVlc), _console, _playlist);
+                _mediaPlayer = new MediaPlayerWrapper(new MediaPlayer(libVlc), _console, _playlist, dataModel);
+      
                 var commandFactory =
                     new MediaFactory(fileSystem.Directory,
                         fileSystem.Path, _console, libVlc);
                 var processor = new IdProcessor(commandFactory, _console, _playlist, _mediaPlayer);
                 var mainLoop = new MainLoop(_console, processor);
 
+                CreateMqttService(processor, arguments, dataModel);
+
                 var gpioDriverFactory = new GpioDriverFactory(_console, _console);
                 var driver = gpioDriverFactory.Create();
                 var controller = new GpioController(PinNumberingScheme.Logical, driver);
-                var volDownButton = new VolumeDownButton(controller, _mediaPlayer, _console);
-                var volUpButton = new VolumeUpButton(controller, _mediaPlayer, _console);
-                var previousButton = new PreviousButton(controller, _mediaPlayer, _playlist, _console);
-                var nextButton = new NextButton(controller, _mediaPlayer, _playlist, _console);
-                var playPlauseButton = new PlayPauseButton(controller, _mediaPlayer, _console);
+                var volDownButton = new VolumeDownButton(controller, _mediaPlayer, _console, dataModel);
+                var volUpButton = new VolumeUpButton(controller, _mediaPlayer, _console, dataModel);
+                var previousButton = new PreviousButton(controller, _mediaPlayer, _playlist, _console, dataModel);
+                var nextButton = new NextButton(controller, _mediaPlayer, _playlist, _console, dataModel);
+                var playPlauseButton = new PlayPauseButton(controller, _mediaPlayer, _console, dataModel);
 
 #pragma warning disable 4014
                 volDownButton.Activate(4);
@@ -74,6 +80,63 @@ namespace JukeCore
             {
                 _console.WriteLine(e.Message);
             }
+        }
+
+        private static void CreateMqttService(IdProcessor processor,
+            CommandLineArguments arguments, JukeCoreDataModel dataModel)
+        {
+
+            if (string.IsNullOrWhiteSpace(arguments.MqttPrefix))
+            {
+                _console.WriteLine("No mqtt ip was given. Mqtt service disabled.");
+                return;
+            }
+
+            _console.WriteLine($"Starting mqtt service with prefix {arguments.MqttPrefix}");
+
+            var mqttFactory = new MqttFactory();
+            var mqttClient = mqttFactory.CreateMqttClient();
+            var optionBuilder = new MqttClientOptionsBuilder()
+                .WithClientId("jukeCore")
+                .WithTcpServer(arguments.MqttBrokerIp, arguments.MqttPort)
+                .WithCleanSession();
+
+            if (!string.IsNullOrEmpty(arguments.MqttUsername) &&
+                !string.IsNullOrEmpty(arguments.MqttPassword))
+            {
+                //optionBuilder = optionBuilder.WithCredentials("fhem", "M9x#X/bqx")
+                optionBuilder = optionBuilder.WithCredentials(arguments.MqttUsername, arguments.MqttPassword);
+            }
+
+            var options = optionBuilder.Build();
+
+            var mqttPrefix = arguments.MqttPrefix;
+            var volumeTopicHandler = new VolumeTopicHandler(_mediaPlayer, mqttPrefix);
+            var playIdTopicHandler = new PlayIdTopicHandler(processor, mqttPrefix, arguments.JukeCoreMediaPath);
+            var commandTopicHandler = new CommandTopicHandler(_mediaPlayer, _playlist, mqttPrefix);
+            List<ITopicHandler> topicHandlers = new List<ITopicHandler>
+            {
+                volumeTopicHandler,
+                playIdTopicHandler,
+                commandTopicHandler
+            };
+
+            // only data model members are supported in the value / nameof clause ...
+            var topicsToPublish = new Dictionary<string, string>
+            {
+                {$"jukeCore/{arguments.MqttPrefix}/status/playback", nameof(dataModel.PlaybackState)},
+                {$"jukeCore/{arguments.MqttPrefix}/status/mediaDurationMs", nameof(dataModel.MediaDurationMs)},
+                {$"jukeCore/{arguments.MqttPrefix}/status/mediaFilename", nameof(dataModel.MediaFilename)},
+                {$"jukeCore/{arguments.MqttPrefix}/status/mediaPositionMs", nameof(dataModel.MediaPositionMs)},
+                {$"jukeCore/{arguments.MqttPrefix}/status/volumePercent", nameof(dataModel.VolumePercent)},
+                {$"jukeCore/{arguments.MqttPrefix}/status/buttons/volumeDown", nameof(dataModel.VolumeDownButtonState)},
+                {$"jukeCore/{arguments.MqttPrefix}/status/buttons/volumeUp", nameof(dataModel.VolumeUpButtonState)},
+                {$"jukeCore/{arguments.MqttPrefix}/status/buttons/previous", nameof(dataModel.PreviousButtonState)},
+                {$"jukeCore/{arguments.MqttPrefix}/status/buttons/next", nameof(dataModel.NextButtonState)},
+                {$"jukeCore/{arguments.MqttPrefix}/status/buttons/playPause", nameof(dataModel.PlayPauseButtonState)},
+            };
+
+        var mqttService = new MqttService(mqttClient, options, _console, topicHandlers, topicsToPublish, dataModel, 2000, mqttPrefix);
         }
     }
 }
